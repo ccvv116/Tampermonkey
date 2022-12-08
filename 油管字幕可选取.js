f6ff97052e257afea1297d7b25a4224baf146a39
czopo <634605333@qq.com>
2022-12-08 17:12:08 +0800

    Create 油管字幕可选取.js
    create: 创建油管字幕可选取脚本

---
+++ 油管字幕可选取.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         油管字幕可选取
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.youtube.com/watch*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    let css=`
+      .caption-window{
+        -moz-user-select: text !important;;
+        -ms-user-select: text !important;
+        -webkit-user-select: text !important;
+        cursor:text !important;
+      }
+    `
+    GM_addStyle(css)
+})();
